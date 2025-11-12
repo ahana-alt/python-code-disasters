68776cf58e9fe6c3002a73a092826d99d0b9044d
ahana-alt <ahanaa@andrew.cmu.edu>
2025-11-12 16:12:42 -0500

    small change

--- python/check_ip.py
+++ python/check_ip.py
@@ -1,25 +1,26 @@
 from multiprocessing import Process
 
 def split_list(alist, wanted_parts=1):
+    """Split a list into equal parts."""
     length = len(alist)
-    return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts] for i in range(wanted_parts)]
+    return [
+        alist[i * length // wanted_parts: (i + 1) * length // wanted_parts] 
+        for i in range(wanted_parts)
+    ]
 
 def check_ip(iplist, masklist):
+    """Check IP addresses using multiprocessing."""
     threads = 16
-    workList = {}
-
-    workList[0], workList[1], workList[2], workList[3], \
-    workList[4], workList[5], workList[6], workList[7], \
-    workList[8], workList[9], workList[10], workList[11], \
-    workList[12], workList[13], workList[14], \
-    workList[15] = split_list(iplist, 16)
-
+    # Refactored: cleaner list splitting
+    work_list = split_list(iplist, threads)
+    
     processes = [
-        Process(target=include_worker, args=(workList[i], masklist)) for i
-        in range(0, threads)]
-
-    for p in processes:
-        p.start()
-
-    for p in processes:
-        p.join()
+        Process(target=include_worker, args=(work_list[i], masklist)) 
+        for i in range(threads)
+    ]
+    
+    for process in processes:
+        process.start()
+    
+    for process in processes:
+        process.join()
